942538d1844de9e2ea665f405c739986e20d557f
bbrtj <bbrtj.pro@gmail.com>
2023-07-29 21:15:49 +0200

    Add an example test

---
+++ ex/fib.t.pas
@@ -0,0 +1,44 @@
+{
+	build with: make examples
+	run with: prove build/fib.t
+}
+program FibonacciTest;
+
+{$mode objfpc}{$H+}{$J-}
+
+uses TAP;
+
+type
+	TFibonacciHistory = Array [0 .. 1] of UInt32;
+
+var
+	vLastFibonacci: TFibonacciHistory = (0, 1);
+
+function GetNextFibonacciNumber(): UInt32;
+begin
+	result := vLastFibonacci[0] + vLastFibonacci[1];
+	vLastFibonacci[0] := vLastFibonacci[1];
+	vLastFibonacci[1] := result;
+end;
+
+begin
+	// Subtests are useful to subdivide your test
+	SubtestBegin('Should return first 5 Fibonacci numbers');
+	TestIs(GetNextFibonacciNumber(), 1, 'Fibonacci number 1 ok');
+	TestIs(GetNextFibonacciNumber(), 2, 'Fibonacci number 2 ok');
+	TestIs(GetNextFibonacciNumber(), 3, 'Fibonacci number 3 ok');
+	TestIs(GetNextFibonacciNumber(), 5, 'Fibonacci number 4 ok');
+	TestIs(GetNextFibonacciNumber(), 8, 'Fibonacci number 5 ok');
+	SubtestEnd;
+
+	// You can mark subtest as skipped or todo, which will not produce any
+	// output for its tests (even if implemented)
+	SubtestBegin('Should return the sixth Fibonacci number');
+	Plan(stTodo, 'procrastinating...');
+	TestIs(GetNextFibonacciNumber(), 12, 'did I get this right?');
+	SubtestEnd;
+
+	// you have to run Plan or DoneTesting in your test program
+	DoneTesting;
+end.
+
--- makefile
+++ makefile
@@ -2,14 +2,16 @@ FPC ?= fpc
 BUILD_DIR ?= build
 SOURCE_DIR ?= src
 FPC_FLAGS ?= -v0web -Sic -FE$(BUILD_DIR) -Fu$(SOURCE_DIR)
-O_LEVEL ?= 1
 TEST_RUNNER ?= prove
 
 build: prepare
-	for file in t/*.t.pas; do $(FPC) $(FPC_FLAGS) -Fut/src -O$(O_LEVEL) $${file}; done
+	for file in t/*.t.pas; do $(FPC) $(FPC_FLAGS) -Fut/src $${file}; done
 
 test: build
 	$(TEST_RUNNER) build
+
+examples: prepare
+	for file in ex/*.t.pas; do $(FPC) $(FPC_FLAGS) $${file}; done
 
 prepare:
 	mkdir -p $(BUILD_DIR)
