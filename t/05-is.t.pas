07a5153caa7b39ee2b6ee022efffba39d749df41
bbrtj <bbrtj.pro@gmail.com>
2023-07-29 19:33:32 +0200

    Test for boolean equality

--- t/05-is.t.pas
+++ t/05-is.t.pas
@@ -14,6 +14,12 @@ begin
 	TestIs('??', '?', 'strings 0');
 end;
 
+procedure RunTestBooleans();
+begin
+	TestIs(False, False, 'booleans 1');
+	TestIs(True, False, 'booleans 0');
+end;
+
 begin
 	TAPTester.Hijack;
 	RunTestIntegers;
@@ -29,6 +35,14 @@ begin
 
 	TestIs(TAPTester.Lines[0], 'ok 1 - strings 1', 'strings 1 test ok');
 	TestIs(TAPTester.Lines[1], 'not ok 2 - strings 0', 'strings 0 test ok');
+	TestOk(TAPTester.Lines.Count > 2, 'lines describing the failure ok');
+
+	TAPTester.Hijack;
+	RunTestBooleans;
+	TAPTester.Release;
+
+	TestIs(TAPTester.Lines[0], 'ok 1 - booleans 1', 'booleans 1 test ok');
+	TestIs(TAPTester.Lines[1], 'not ok 2 - booleans 0', 'booleans 0 test ok');
 	TestOk(TAPTester.Lines.Count > 2, 'lines describing the failure ok');
 
 	DoneTesting;
