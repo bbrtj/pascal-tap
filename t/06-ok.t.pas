9c6ede07f36418605b9e316b5fbd21a4fd9e482c
bbrtj <bbrtj.pro@gmail.com>
2023-07-30 19:31:32 +0200

    Test for not-described output

--- t/06-ok.t.pas
+++ t/06-ok.t.pas
@@ -5,6 +5,7 @@ uses TAP, Tester;
 procedure RunTest();
 begin
 	TestOk(True, 'test 1');
+	TestOk(True);
 	TestOk(False, 'test 2');
 end;
 
@@ -13,9 +14,10 @@ begin
 	RunTest;
 	TAPTester.Release;
 
-	TestIs(TAPTester.Lines.Count, 6, 'line count ok');
+	TestIs(TAPTester.Lines.Count, 7, 'line count ok');
 	TestIs(TAPTester.Lines[0], 'ok 1 - test 1', 'first test ok');
-	TestIs(TAPTester.Lines[1], 'not ok 2 - test 2', 'second test ok');
+	TestIs(TAPTester.Lines[1], 'ok 2', 'second test ok');
+	TestIs(TAPTester.Lines[2], 'not ok 3 - test 2', 'third test ok');
 	DoneTesting;
 end.
 
