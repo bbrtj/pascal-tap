c03b01e8eec04e211de06392b08f88dbf8d61ac2
bbrtj <bbrtj.pro@gmail.com>
2023-08-01 06:19:43 +0200

    Document TAPSuite

--- src/tapsuite.pas
+++ src/tapsuite.pas
@@ -1,4 +1,11 @@
 {
+	TAPSuite is a framework for execution of tests using pascal-tap. The goal
+	is to group multiple test cases into one program which compiles and
+	executes faster.
+
+	TODO: parallelization.
+
+	For usage examples, see the tests of this module.
 }
 unit TAPSuite;
 
@@ -20,19 +27,34 @@ type
 
 	TTAPScenarios = specialize TFPGList<TTAPScenario>;
 
+	{
+		Implementing this interface marks the entire suite as skipped.
+	}
 	ITAPSuiteSkip = interface
 	['{06671f72-3010-11ee-86c0-002b67685373}']
 	end;
 
+	{
+		Implementing this interface marks the entire suite as essential.
+		Essential suites will halt the execution if they bail or error out.
+	}
 	ITAPSuiteEssential = interface
 	['{98a0e1bd-301c-11ee-86c0-002b67685373}']
 	end;
 
+	{
+		Extend this abstract class and add an object of it into TAPSuites list
+		(with TAPSuites.Add) for the test to be run during RunAllSuites.
+	}
 	TTAPSuite = class abstract
 	protected
 		FScenarios: TTAPScenarios;
 		FSuiteName: String;
 
+		{
+			Add a scenario to be run in this suite. Should be run in the constructor.
+			A scenario is a method (procedure).
+		}
 		procedure Scenario(const vRunner: TTAPScenarioRunner; const vName: String = '');
 
 	public
@@ -51,6 +73,9 @@ type
 var
 	TAPSuites: TTAPSuites;
 
+{
+	Run all registered test suites.
+}
 procedure RunAllSuites();
 
 implementation
