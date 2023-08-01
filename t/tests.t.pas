8bea27158b0a49903a5d1b44f5b86316b41312a3
bbrtj <bbrtj.pro@gmail.com>
2023-08-01 06:11:38 +0200

    Supply missing FlowControl tests in runner

--- t/tests.t.pas
+++ t/tests.t.pas
@@ -1,9 +1,12 @@
 program Tests;
 
-uses TAPSuite,
-	BasicTests, HelpersTests, SubtestsTests;
+uses TAP, TAPSuite,
+	BasicTests, HelpersTests, SubtestsTests, FlowControlTests;
 
 begin
+	// BasicTests unit has 2 suites
+	Plan(5);
+
 	RunAllSuites;
 end.
 
